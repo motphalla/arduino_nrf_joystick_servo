48d32674436cd078887feec696ea9c0b1cf83d1a
motphalla <31525363+motphalla@users.noreply.github.com>
2021-04-24 12:23:44 +0900

    Add files via upload

---
+++ joystick_receiver.ino
@@ -0,0 +1,29 @@
+//Receiver 
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+RF24 radio(9, 10); // CE, CSN
+const byte addresses[][6] = {"00001", "00002"};
+Servo myServo;
+void setup() {
+  myServo.attach(5);
+  radio.begin();
+  radio.openWritingPipe(addresses[0]); // 00001
+  radio.openReadingPipe(1, addresses[1]); // 00002
+  radio.setPALevel(RF24_PA_MIN);
+}
+void loop() {
+  delay(5);
+  radio.startListening();
+  if ( radio.available()) {
+    while (radio.available()) {
+      int angleV = 0;
+      radio.read(&angleV, sizeof(angleV));
+      myServo.write(angleV);
+    }
+    delay(5);
+    radio.stopListening();
+  }
+}
